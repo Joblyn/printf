bc275b04c466069518104cfce79aee5e70f3bbd4
Job Oaikhenah <oaikhenahjob@gmail.com>
2023-04-26 18:37:33 -0700

    hanlde more specifiers

--- _printf.c
+++ _printf.c
@@ -3,9 +3,9 @@
 /**
  * _printf - write output to stdout
  * @format: character string
- * Return: hte number of characters printed
+ *
+ * Return: the number of characters printed
  */
-
 int _printf(const char *format, ...)
 {
 	unsigned int i = 0, len = 0, ibuf = 0;
@@ -23,15 +23,29 @@ int _printf(const char *format, ...)
 		if (format[i] == '%')
 		{
 			if (format[i + 1] == '\0')
-			{
-				print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
 				return (-1);
 			}
 			else
-			{
-				function = get_print_func(format, i + 1);
-				if ()
-			}
+			{	function = get_print_func(format, i + 1);
+				if (function == NULL)
+				{
+					if (format[i + 1] == ' ' && !format[i + 2])
+						return (-1);
+					handl_buf(buffer, format[i], ibuf), len++, i--;
+				}
+				else
+				{
+					len += function(arguments, buffer, ibuf);
+					i += ev_print_func(format, i + 1);
+				}
+			} i++;
 		}
+		else
+			handl_buf(buffer, format[i], ibuf), len++;
+		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
+			;
 	}
+	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+	return (len);
 }
